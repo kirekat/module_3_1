9d3410773999fe60bb3fc29c4888baa0349d8348
kirekat <kec85@mail.ru>
2024-09-21 12:54:47 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains (string, list_to_search):
+    count_calls ()
+    return string.upper() in [s.lower() for s in list_to_search]
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
